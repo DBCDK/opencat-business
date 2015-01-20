46f690b12c2ebd9d94986fa6a05afb1a57691bf0
stp <stp@dbc.dk>
2015-01-20 14:55:47 +0000

    Fixed some execution errors on guesstimate.
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@80599 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- bin/run_unittests.sh
+++ bin/run_unittests.sh
@@ -20,4 +20,4 @@ do
     search_args=$search_args"-a file:$DISTRIBUTIONS_PATH/$i "
 done
 
-$SHELL_CMD "$search_args -l $LOG_FILE -L $LOG_LEVEL -c main() run_unittests.js"
+$SHELL_CMD $search_args -l $LOG_FILE -L $LOG_LEVEL -c main\(\) run_unittests.js
