70f97ad6ef6684f53893b1ba22a749bb85e06a07
stp <stp@dbc.dk>
2015-02-04 09:20:18 +0000

    Removed wiremock that is not used.
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@81090 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- bin/run_unittests.sh
+++ bin/run_unittests.sh
@@ -28,18 +28,4 @@ WIREMOCK_JAR=../wiremock-1.50-standalone.jar
 WIREMOCK_SOLR_PORT=12100
 WIREMOCK_SOLR_DIR=$WIREMOCK_ROOTDIR/solr
 
-#cd $WIREMOCK_SOLR_DIR
-#nohub java -jar $WIREMOCK_JAR --verbose --port $WIREMOCK_SOLR_PORT > wiremock.out 2>&1 &
-#WIREMOCK_SOLR_PID=$!
-#echo "Starting wiremock on port $WIREMOCK_SOLR_PORT with pid $WIREMOCK_SOLR_PID"
-#cd -
-
-#sleep 2
-#ps aux | grep wiremock
-
-# curl --verbose "http://localhost:12100/solr/raw-repo-index/select?q=marc.001a%3A%2A&indent=true&wt=json"
-#               http://localhost:12100/solr/raw-repo-index/select?q=marc.001a%3A%2A&indent=true&wt=json
-
 $SHELL_CMD $search_args -l $LOG_FILE -L $LOG_LEVEL -c main\(\) run_unittests.js
-
-#kill -9 $WIREMOCK_SOLR_PID
