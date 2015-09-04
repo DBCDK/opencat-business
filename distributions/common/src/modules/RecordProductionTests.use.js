e872443bc71ab55ca2be93b422879c7623ee66ea
stp <stp@dbc.dk>
2015-09-04 10:15:26 +0000

    Fixed bug 18968: Skæringsugedag for poster under produktion er sat til mandag.
    Fixed unit tests.
    
    
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@89255 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- distributions/common/src/modules/RecordProductionTests.use.js
+++ distributions/common/src/modules/RecordProductionTests.use.js
@@ -56,46 +56,12 @@ UnitTest.addFixture( "RecordProduction.__extractWeekAndYear", function() {
 
 //-----------------------------------------------------------------------------
 UnitTest.addFixture( "RecordProduction.__calculateFirstProductionDate", function() {
-    var expected = new Date( 2007, 4, 28 );
-    SafeAssert.equal( "Week: 23, 2007", RecordProduction.__calculateFirstProductionDate( 23, 2007 ), expected );
-
-    expected = new Date( 2006, 11, 25 );
-    SafeAssert.equal( "Week: 1, 2007", RecordProduction.__calculateFirstProductionDate( 1, 2007 ), expected );
-} );
-
-//-----------------------------------------------------------------------------
-/*
-UnitTest.addFixture( "RecordProduction.javaCalendar", function() {
-    var Calendar = Packages.java.util.Calendar;
     var SimpleDateFormat = Packages.java.text.SimpleDateFormat;
-
-    var cal = Calendar.getInstance();
-    cal.set( Calendar.YEAR, 2007 );
-    cal.set( Calendar.WEEK_OF_YEAR, 23 );
-    cal.set( Calendar.DAY_OF_WEEK, Calendar.MONDAY );
-
     var sdf = new SimpleDateFormat( "dd/MM-yyyy");
 
-    SafeAssert.equal( "Calendar: 232007", sdf.format( cal.getTime() ) + '', "04/06-2007" );
+    var expected = new Date( 2007, 5, 1 );
+    SafeAssert.equal( "Week: 23, 2007", sdf.format( RecordProduction.__calculateFirstProductionDate( 23, 2007 ) ), sdf.format( expected ) );
 
-    var date = new Date( cal.get( Calendar.YEAR ), cal.get( Calendar.MONTH ), cal.get( Calendar.DAY_OF_MONTH ) );
-    SafeAssert.equal( "Date: 232007", date.toDateString(), "Mon Jun 04 2007" );
-
-    var curDate = new Date( 2015, 7, 26 );
-    SafeAssert.equal( "curDate 1", curDate.toDateString(), "Wed Aug 26 2015" );
-    SafeAssert.that( "Compare 1", curDate.getTime() > date.getTime() );
-
-    curDate = new Date( 2007, 5, 4 );
-    SafeAssert.equal( "curCal 2", curDate.toDateString(), "Mon Jun 04 2007" );
-    SafeAssert.that( "Compare 2", curDate.getTime() === date.getTime() );
-
-    curDate = new Date( 2007, 5, 3 );
-    SafeAssert.equal( "curCal 3", curDate.toDateString(), "Sun Jun 03 2007" );
-    SafeAssert.that( "Compare 3", curDate.getTime() < date.getTime() );
-
-    curDate = new Date;
-    SafeAssert.equal( "Day", curDate.getDate(), 26 );
-    SafeAssert.equal( "Month", curDate.getMonth(), 7 );
-    SafeAssert.equal( "Year", curDate.getFullYear(), 2015 );
+    expected = new Date( 2006, 11, 29 );
+    SafeAssert.equal( "Week: 1, 2007", sdf.format( RecordProduction.__calculateFirstProductionDate( 1, 2007 ) ), sdf.format( expected ) );
 } );
-*/
