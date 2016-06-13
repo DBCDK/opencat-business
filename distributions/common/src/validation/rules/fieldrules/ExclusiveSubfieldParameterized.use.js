886c340137b736da077bbe32415509e5ee2b6256
Anders Thrane Michelsen <atm@dbc.dk>
2016-06-13 13:16:07 +0000

    #1930 Ny valideringsregel: "conflictingSubfields" - kun inden for samme felt
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@101231 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ distributions/common/src/validation/rules/fieldrules/ExclusiveSubfieldParameterized.use.js
@@ -0,0 +1,81 @@
+//-----------------------------------------------------------------------------
+use( "Exception" );
+use( "Log" );
+use( "ResourceBundle" );
+use( "ResourceBundleFactory" );
+use( "ValidateErrors" );
+use( "ValueCheck" );
+
+//-----------------------------------------------------------------------------
+EXPORTED_SYMBOLS = ['ExclusiveSubfieldParameterized'];
+//-----------------------------------------------------------------------------
+
+var ExclusiveSubfieldParameterized = function () {
+    var BUNDLE_NAME = "validation";
+
+    /**
+     * exclusiveSubfieldParameterized is used to validate that only one fields defined by the params are present 
+     * @syntax ExclusiveSubfieldParameterized.validateField(  record, field, params  )
+     * @param {object} record
+     * @param {object} field
+     * @param {object} params list of exclusive values
+     * @return {object}
+     * @name ExclusiveSubfieldParameterized.validateField
+     * @method
+     */
+    function validateField ( record, field, params ) {
+        Log.trace( "Enter -- ExclusiveSubfieldParameterized.validateField" );
+        try {
+            var result = [];
+            var fieldName, subFields;
+            var bundle = ResourceBundleFactory.getBundle( BUNDLE_NAME );
+
+            ValueCheck.checkThat( "params", params ).type( "object" );
+
+            ValueCheck.checkValue( "params.fieldName", params.fieldName ).is.defined
+                .and.has.type( "string" )
+                .and.value.that.is.not.equalTo( "" )
+                .and.value.that.is.not.equalTo( null );
+
+            ValueCheck.check( "params.subfields", params.subfields ).instanceOf( Array );
+
+            // Validate params
+            if (params.fieldName.length == 3) {
+                fieldName = params.fieldName;
+            } else {
+                Log.debug(ResourceBundle.getString(bundle, "conflictingSubfieldsParameterized.params.field.error"), params.fieldName);
+                throw ResourceBundle.getStringFormat(bundle, "conflictingSubfieldsParameterized.params.field.error", params.fieldName);
+            }
+
+            if (params.subfields.length >= 2) {
+                subFields = params.subfields;
+            } else {
+                Log.debug(ResourceBundle.getString(bundle, "conflictingSubfieldsParameterized.params.subfields.error"), params.subfields);
+                throw ResourceBundle.getStringFormat(bundle, "conflictingSubfieldsParameterized.params.subfields.error", params.subfields);
+            }
+
+            var foundFirstSubfield = false;
+
+            if (field.name == fieldName) {
+                for (var f = 0; f < field.subfields.length; f++) {
+                    if (subFields.indexOf(field.subfields[f].name) > -1) {
+                        if (foundFirstSubfield) { // second match -> return error
+                            result.push( ValidateErrors.fieldError( "TODO:fixurl", ResourceBundle.getStringFormat( bundle, "exclusive.subfield.paramterized.rule.error", field.subfields[f].name, subFields ) ) );
+                            return result;
+                        } else {
+                            foundFirstSubfield = true; // First match
+                        }
+                    }
+                }
+            }
+            
+            return result;
+        } finally {
+            Log.trace( "Exit -- ExclusiveSubfieldParameterized.validateField" );
+        }
+    }
+    return {
+        'BUNDLE_NAME': BUNDLE_NAME,
+        'validateField': validateField
+    };
+}();
---
+++ distributions/common/src/validation/rules/fieldrules/ExclusiveSubfieldParameterizedTest.use.js
@@ -0,0 +1,81 @@
+//-----------------------------------------------------------------------------
+use( "UnitTest" );
+use( "SafeAssert" );
+use( "ExclusiveSubfieldParameterized" );
+//-----------------------------------------------------------------------------
+UnitTest.addFixture( "ExclusiveSubfieldParameterized", function( ) {
+    var bundle = ResourceBundleFactory.getBundle( ExclusiveSubfieldParameterized.BUNDLE_NAME );
+    var record = {};
+    var params = {fieldName: '900', subfields: ['w', 'z']};
+
+    var field1 = {
+        name : '900', indicator : '00', subfields : [{
+            name : "a", value : "aVal"
+        }]
+    };
+    SafeAssert.equal( "1 ExclusiveSubfieldParameterized test ok", ExclusiveSubfieldParameterized.validateField( record, field1, params ), [] );
+
+    var field11 = {
+        name : '900', indicator : '00', subfields : [{
+            name : "a", value : "aVal"
+        }, {
+            name : "w", value : "aVal"
+        }]
+    };
+    SafeAssert.equal( "11 ExclusiveSubfieldParameterized test ok", ExclusiveSubfieldParameterized.validateField( record, field11, params ), [] );
+
+    var field12 = {
+        name : '900', indicator : '00', subfields : [{
+            name : "a", value : "aVal"
+        }, {
+            name : "z", value : "aVal"
+        }]
+    };
+    SafeAssert.equal( "12 ExclusiveSubfieldParameterized test ok", ExclusiveSubfieldParameterized.validateField( record, field12, params ), [] );
+
+    var field13 = {
+        name : '900', indicator : '00', subfields : [{
+            name : "w", value : "aVal"
+        }]
+    };
+    SafeAssert.equal( "13 ExclusiveSubfieldParameterized test ok", ExclusiveSubfieldParameterized.validateField( record, field13, params ), [] );
+
+    var field14 = {
+        name : '100', indicator : '00', subfields : [{
+            name : "a", value : "aVal"
+        }]
+    };
+    SafeAssert.equal( "14 ExclusiveSubfieldParameterized test ok", ExclusiveSubfieldParameterized.validateField( record, field14, params ), [] );
+
+    var field2 = {
+        name : '900', indicator : '00', subfields : [{
+            name : "z", value : "aVal"
+        }, {
+            name : "w", value : "bVal"
+        }]
+    };
+    var error2 = [ValidateErrors.fieldError( "TODO:fixurl", ResourceBundle.getStringFormat( bundle, "exclusive.subfield.parameterized.rule.error", 'w', ['w', 'z'] ) ) ];
+    SafeAssert.equal( "2 ExclusiveSubfieldParameterized test not-ok", ExclusiveSubfieldParameterized.validateField( record, field2, params ), error2 );
+
+    var field3 = {
+        name : '900', indicator : '00', subfields : [{
+            name : "a", value : "iVal"
+        }, {
+            name : "b", value : "tVal"
+        }, {
+            name : "c", value : "eVal"
+        }, {
+            name : "d", value : "xVal"
+        }, {
+            name : "w", value : "bVal"
+        }, {
+            name : "z", value : "aVal"
+        }]
+    };
+    var error3 = [
+        ValidateErrors.fieldError( "TODO:fixurl", ResourceBundle.getStringFormat( bundle, "exclusive.subfield.parameterized.rule.error", "z", ['w', 'z'] ) )
+    ];
+    SafeAssert.equal( "3 ExclusiveSubfieldParameterized test not-ok", ExclusiveSubfieldParameterized.validateField( record, field3, params ), error3 );
+
+
+} );
