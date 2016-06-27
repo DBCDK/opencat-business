cd09be67b4d66447f31494ac639db9e262bd318f
Anders Thrane Michelsen <atm@dbc.dk>
2016-06-27 06:55:15 +0000

    #1892 - Der er en uskøn blanding af s10 og 996 flere steder i updateservice, ocb-tools og iscrum-commons. Da der ikke kan komme poster med s10 fra basis, så skal denne kode fjernes da den forstyrrer. - Implementeret reviewkommentarer
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@101938 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- distributions/dataio/src/modules/DBCAuthenticatorTests.use.js
+++ distributions/dataio/src/modules/DBCAuthenticatorTests.use.js
@@ -94,30 +94,6 @@ UnitTest.addFixture("DBCAuthenticator.authenticateRecord", function () {
     record = new Record();
     record.fromString(
         StringUtil.sprintf("001 00 *a 1 234 567 8 *b %s\n", UpdateConstants.COMMON_AGENCYID) +
-        "004 00 *a e *r n"
-    );
-    Assert.equalValue("Update record with 996 in current record",
-        callFunction(record, "netpunkt", LOGIN_AGENCY_ID),
-        []);
-    RawRepoClientCore.clear();
-
-    curRecord = new Record();
-    curRecord.fromString(
-        StringUtil.sprintf("001 00 *a 1 234 567 8 *b %s\n", UpdateConstants.COMMON_AGENCYID) +
-        "004 00 *a e *r n\n" +
-        StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID)
-    );
-    RawRepoClientCore.addRecord(curRecord);
-    curRecord = new Record();
-    curRecord.fromString(
-        StringUtil.sprintf("001 00 *a 1 234 567 8 *b %s\n", UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID) +
-        StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID)
-    );
-    RawRepoClientCore.addRecord(curRecord);
-
-    record = new Record();
-    record.fromString(
-        StringUtil.sprintf("001 00 *a 1 234 567 8 *b %s\n", UpdateConstants.COMMON_AGENCYID) +
         "004 00 *a e *r n" +
         StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID)
     );
@@ -169,7 +145,6 @@ UnitTest.addFixture("DBCAuthenticator.authenticateRecord", function () {
     record.fromString(
         StringUtil.sprintf("001 00 *a 1 234 567 8 *b %s\n", UpdateConstants.COMMON_AGENCYID) +
         "004 00 *a e *r n\n" +
-        StringUtil.sprintf("996 00 *a %s\n", UpdateConstants.COMMON_AGENCYID) +
         StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID)
     );
     Assert.equalValue("Update record with 996 in current record. 996 is presented and unchanged in new record.",
@@ -198,7 +173,6 @@ UnitTest.addFixture("DBCAuthenticator.recordDataForRawRepo", function () {
     record = RecordUtil.createFromString([
         StringUtil.sprintf("001 00 *a 1 234 567 8 *b %s", UpdateConstants.COMMON_AGENCYID),
         "004 00 *a e *r n",
-        StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID),
         StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID)
     ].join("\n"));
 
@@ -212,7 +186,6 @@ UnitTest.addFixture("DBCAuthenticator.recordDataForRawRepo", function () {
     record = RecordUtil.createFromString([
         StringUtil.sprintf("001 00 *a 1 234 567 8 *b %s", UpdateConstants.COMMON_AGENCYID),
         "004 00 *a e *r n",
-        StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID),
         StringUtil.sprintf("996 00 *a %s", UpdateConstants.COMMON_AGENCYID)
     ].join("\n"));
 
