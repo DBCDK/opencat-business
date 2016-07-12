c80f597f575b3045e36cf4b75d900f791c0e6523
Morten von Staffeldt <mvs@dbc.dk>
2016-07-12 08:44:23 +0000

    userstory 1951 , added rule FieldsIndicatorMakesSubfieldRepeatable
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@102441 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ distributions/common/src/validation/rules/fieldrules/FieldsIndicatorMakesSubfieldRepeatable.use.js
@@ -0,0 +1,108 @@
+//-----------------------------------------------------------------------------
+use( "Exception" );
+use( "Log" );
+use( "ResourceBundle" );
+use( "ResourceBundleFactory" );
+use( "ValidateErrors" );
+use( "ValueCheck" );
+
+//-----------------------------------------------------------------------------
+EXPORTED_SYMBOLS = ['FieldsIndicatorMakesSubfieldRepeatable'];
+//-----------------------------------------------------------------------------
+
+var FieldsIndicatorMakesSubfieldRepeatable = function () {
+    var BUNDLE_NAME = "validation";
+
+    /**
+     * FieldsIndicatorMakesSubfieldRepeatable checks that if a given indicator is not present , then the subfield cannot be repeatable
+     * Meaning if the indicator macthes the value from the list , they are allowed to be repeated. 
+     * Note this rule does not negate the the normal rule of repeatable on a given subfield
+     * So to use this rule you have to make the subfields repeatable and then add this rule
+     * 
+     * @syntax FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params )
+     * @param {object} record
+     * @param {object} field
+     * @param {object} params must contain an object with two properties, indicators, subfieldNames.
+     * Both properties must be lists. The indicator list denotes which indicator values makes the list of subfields repeatable.
+     * @return {object}
+     * @name FieldsIndicatorMakesSubfieldRepeatable.validateField
+     * @method
+     */
+
+    function validateField ( record, field, params, settings ) {
+        Log.trace( "Enter - FieldsIndicatorMakesSubfieldRepeatable.validateField( ", record, ", ", field, ", ", params, ", ", settings, " )" );
+
+        var result = [];
+        try {
+            ValueCheck.check( "params.indicators", params.indicators );
+            ValueCheck.check( "params.indicators", params.indicators).instanceOf( Array );
+            ValueCheck.checkThat( "params.indicators", params.indicators.length ).is.greaterThan(0);
+
+            ValueCheck.check( "params.subfieldNames", params.subfieldNames );
+            ValueCheck.check( "params.subfieldNames", params.subfieldNames ).instanceOf( Array );
+
+
+            if ( !__fieldIndicatorInParams( params, field ) ) {
+
+                var subfieldMap = __getSubfieldMap( field.subfields );
+                params.subfieldNames.forEach( function ( subfield ) {
+                    if ( subfieldMap.hasOwnProperty( subfield ) && subfieldMap[subfield].isRepeated === true ) {
+                        result.push( __getError( params.indicators, subfield) );
+                    }
+                } );
+            }
+            return result;
+        } finally {
+            Log.trace( "Exit - FieldsIndicatorMakesSubfieldRepeatable.validateField(): ", result );
+        }
+    }
+
+    function __getError (indicators, subfieldName) {
+        Log.trace( "Enter - FieldsIndicatorMakesSubfieldRepeatable.____getSubfieldMap(): ", indicators ,subfieldName );
+        var res ="";
+       try {
+            var bundle = ResourceBundleFactory.getBundle( BUNDLE_NAME );
+            var errorMessage = ResourceBundle.getStringFormat( bundle, "field.indicator.subfield.repeated.error", subfieldName, indicators );
+            return res = ValidateErrors.fieldError( "TODO:url", errorMessage );
+        } finally  {
+            Log.trace( "exit - FieldsIndicatorMakesSubfieldRepeatable.____getSubfieldMap(): ", res );
+        }
+    }
+
+
+    function __getSubfieldMap ( subfields ) {
+        Log.trace( "Enter - FieldsIndicatorMakesSubfieldRepeatable.____getSubfieldMap(): ", subfields );
+        var ret = {};
+        try {
+            for ( var i = 0; i < subfields.length; i++ ) {
+                if ( ret.hasOwnProperty( subfields[i].name ) ) {
+                    ret[subfields[i].name]["isRepeated"] = true;
+                } else {
+                    ret[subfields[i].name]= {"isRepeated" : false}
+                }
+            }
+            return ret;
+        } finally {
+            Log.trace( "Exit - FieldsIndicatorMakesSubfieldRepeatable.____getSubfieldMap(): ", ret );
+        }
+    }
+
+    function __fieldIndicatorInParams ( params, field ) {
+        var res = false;
+        try {
+            for ( var i = 0; i < params.indicators.length; ++i ) {
+                if ( field.indicator === params.indicators[i] ) {
+                    return res = true;
+                }
+            }
+        } finally {
+            Log.trace( "Exit - FieldsIndicatorMakesSubfieldRepeatable.__fieldIndicatorInParams(): ", res );
+        }
+    }
+
+    return {
+        'BUNDLE_NAME': BUNDLE_NAME,
+        'validateField': validateField
+    };
+
+}();
---
+++ distributions/common/src/validation/rules/fieldrules/FieldsIndicatorMakesSubfieldRepeatableTests.use.js
@@ -0,0 +1,202 @@
+//-----------------------------------------------------------------------------
+use( "UnitTest" );
+use( "SafeAssert" );
+// use( "Assert" );
+use( "FieldsIndicatorMakesSubfieldRepeatable" );
+
+//-----------------------------------------------------------------------------
+UnitTest.addFixture( "FieldsIndicatorMakesSubfieldRepeatable", function () {
+    var bundle = ResourceBundleFactory.getBundle( FieldsIndicatorMakesSubfieldRepeatable.BUNDLE_NAME );
+    var bundleKey = "field.indicator.subfield.repeated.error";
+
+    var record = {};
+    var field = {
+        name: '001', indicator: '00', subfields: []
+    };
+
+
+    var field = {
+        name: '001', indicator: '00', subfields: []
+    };
+
+    var params = {
+        "indicators": ["01"],
+        "subfieldNames": []
+    };
+
+    SafeAssert.equal( "testing with valid indicator", [], FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+    var field = {
+        name: '001', indicator: '00', subfields: []
+    };
+
+    var params = {
+        "indicators": ["00"],
+        "subfieldNames": ["a"]
+    };
+
+    SafeAssert.equal( "testing with valid subfieldName", [], FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+
+    var field = {
+        name: '001', indicator: '01', subfields: [{
+            name: "a", value: "a1Val"
+        }, {
+            name: "a", value: "a2Val"
+        }, {
+            name: "b", value: "b1Val"
+        }]
+    };
+
+    var params = {
+        "indicators": ["01"],
+        "subfieldNames": ["a"]
+    };
+
+    SafeAssert.equal( "testing with valid indicator 01 and repeated a", [], FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+
+    var field = {
+        name: '001', indicator: '00', subfields: [{
+            name: "a", value: "a1Val"
+        }, {
+            name: "a", value: "a2Val"
+        }, {
+            name: "b", value: "b1Val"
+        }]
+    };
+
+    var params = {
+        "indicators": ["01"],
+        "subfieldNames": ["a"]
+    };
+
+    var errorMessage = ResourceBundle.getStringFormat( bundle, bundleKey, params.subfieldNames, params.indicators );
+    var error = [ValidateErrors.fieldError( "TODO:url", errorMessage )];
+
+    SafeAssert.equal( "testing with invalid indicator and a repeated", error, FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+
+    var field = {
+        name: '001', indicator: '00', subfields: [{
+            name: "a", value: "a1Val"
+        }, {
+            name: "a", value: "a2Val"
+        }, {
+            name: "b", value: "b1Val"
+        }]
+    };
+
+    var params = {
+        "indicators": ["01"],
+        "subfieldNames": ["a"]
+    };
+
+    var errorMessage = ResourceBundle.getStringFormat( bundle, bundleKey, params.subfieldNames, params.indicators );
+    var error = [ValidateErrors.fieldError( "TODO:url", errorMessage )];
+
+    SafeAssert.equal( "testing with invalid indicator and a repeated", error, FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+
+    var field = {
+        name: '860', indicator: '04', subfields: [{
+            name: "c", value: "a1Val"
+        }, {
+            name: "c", value: "a2Val"
+        }, {
+            name: "z", value: "b1Val"
+        }, {
+            name: "z", value: "b1Val"
+        }]
+    };
+
+    var params = {
+        "indicators": ["04"],
+        "subfieldNames": ["c", "z"]
+    };
+
+
+    SafeAssert.equal( "Huins test from userstory 1951", [], FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+
+    var field = {
+        name: '860', indicator: '00', subfields: [{
+            name: "c", value: "a1Val"
+        }, {
+            name: "c", value: "a2Val"
+        }, {
+            name: "z", value: "b1Val"
+        }, {
+            name: "z", value: "b1Val"
+        }]
+    };
+
+    var params = {
+        "indicators": ["04"],
+        "subfieldNames": ["c", "z"]
+    };
+
+    var error = [];
+    for ( var i = 0; i < params.subfieldNames.length; i++ ) {
+        var errorMessage = ResourceBundle.getStringFormat( bundle, bundleKey, params.subfieldNames[i], params.indicators );
+        error.push( ValidateErrors.fieldError( "TODO:url", errorMessage ) );
+    }
+
+    SafeAssert.equal( "Huins test 1 from userstory 1951", error, FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+
+    var field = {
+        name: '860', indicator: '00', subfields: [{
+            name: "c", value: "a1Val"
+        }, {
+            name: "c", value: "a2Val"
+        }, {
+            name: "z", value: "b1Val"
+        }, {
+            name: "z", value: "b1Val"
+        }]
+    };
+
+    var params = {
+        "indicators": ["03", "04", "05"],
+        "subfieldNames": ["c", "z"]
+    };
+
+    params.indicators.forEach( function ( indicator ) {
+        field.indicator = indicator;
+        SafeAssert.equal( "Huins test 2  with valid indicators from userstory 1951", [], FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+    } );
+
+    var field = {
+        name: '860', indicator: '00', subfields: [{
+            name: "c", value: "a1Val"
+        }, {
+            name: "c", value: "a2Val"
+        }, {
+            name: "z", value: "b1Val"
+        }, {
+            name: "z", value: "b1Val"
+        }]
+    };
+
+    var params = {
+        "indicators": ["03", "04", "05"],
+        "subfieldNames": ["c", "z"]
+    };
+
+    var error = [];
+    params.subfieldNames.forEach( function ( sf ) {
+        var errorMessage = ResourceBundle.getStringFormat( bundle, bundleKey, sf, params.indicators );
+        error.push( ValidateErrors.fieldError( "TODO:url", errorMessage ) );
+    } );
+
+    SafeAssert.equal( "Huins test 2 with invalid indicators from userstory 1951", error, FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+
+
+    params.indicators.forEach( function ( indicator ) {
+        field.indicator = indicator;
+        SafeAssert.equal( "Huins test 2  with valid indicators from userstory 1951", [], FieldsIndicatorMakesSubfieldRepeatable.validateField( record, field, params ) );
+    } );
+
+
+} );
