01a5039c4587b5c9a1204cc55265bf924b81c0ca
Thomas Hesselholt Lang <thl@dbc.dk>
2016-07-27 11:20:27 +0000

    More debug output for records
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@102902 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- distributions/fbs/src/modules/FBSClassificationData.use.js
+++ distributions/fbs/src/modules/FBSClassificationData.use.js
@@ -20,31 +20,30 @@ var FBSClassificationData = function() {
         return ClassificationData.hasClassificationData( instance, marc );
     }
 
-    function hasClassificationsChanged( instance, oldMarc, newMarc ) {
-        Log.trace( "Enter - FBSClassificationData.hasClassificationsChanged( ", oldMarc, ", ", newMarc, " )" );
-        Log.trace( "debug - FBSClassificationData.hasClassificationsChanged( ", oldMarc, ", ", newMarc, " )" );
+    function hasClassificationsChanged(instance, oldMarc, newMarc) {
+        Log.trace("Enter - FBSClassificationData.hasClassificationsChanged( ", oldMarc, ", ", newMarc, " )");
+        Log.info("THL Enter - FBSClassificationData.hasClassificationsChanged( ", oldMarc, ", ", newMarc, " )");
 
         var result = null;
         try {
-            var lookup = RecordLookupField.createFromRecord( oldMarc, instance.fields );
+            var lookup = RecordLookupField.createFromRecord(oldMarc, instance.fields);
+            Log.info("THL lookup:\n" + JSON.stringify(lookup, undefined, 3));
 
-            for( var i = 0; i < newMarc.numberOfFields(); i++ ) {
-                var newField = newMarc.field( i );
+            for (var i = 0; i < newMarc.numberOfFields(); i++) {
+                var newField = newMarc.field(i);
 
-                if( !instance.fields.test( newField.name ) ) {
+                if (!instance.fields.test(newField.name)) {
                     continue;
                 }
-
-                if( !RecordLookupField.containsField( lookup, newField ) ) {
-                    Log.debug( "Unable to find field: ", newField );
-                    Log.debug( "Lookup record:\n", oldMarc );
+                if (!RecordLookupField.containsField(lookup, newField)) {
+                    Log.debug("Unable to find field: ", newField);
+                    Log.debug("Lookup record:\n", oldMarc);
                     return result = true;
                 }
             }
             return result = false;
-        }
-        finally {
-            Log.trace( "Exit - FBSClassificationData.hasClassificationsChanged(): ", result );
+        } finally {
+            Log.trace("Exit - FBSClassificationData.hasClassificationsChanged(): ", result);
         }
     }
 
