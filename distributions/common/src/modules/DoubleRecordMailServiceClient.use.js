f3586d884e434ae81d41c1d43244fb214fce117a
Thomas Hesselholt Lang <thl@dbc.dk>
2016-09-14 07:09:57 +0000

    Re-formatted file
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@104136 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- distributions/common/src/modules/DoubleRecordMailServiceClient.use.js
+++ distributions/common/src/modules/DoubleRecordMailServiceClient.use.js
@@ -1,26 +1,21 @@
-//-----------------------------------------------------------------------------
-use( "DoubleRecordMailServiceClientCore" );
-use( "Log" );
+use("DoubleRecordMailServiceClientCore");
+use("Log");
 
-//-----------------------------------------------------------------------------
-EXPORTED_SYMBOLS = [ 'DoubleRecordMailServiceClient' ];
+EXPORTED_SYMBOLS = ['DoubleRecordMailServiceClient'];
 
-//-----------------------------------------------------------------------------
 /**
  * Module to send a mail for the double checker service.
  *
  * @namespace
  * @name DoubleRecordMailServiceClient
  */
-var DoubleRecordMailServiceClient = function() {
-    function sendMessage( subject, body ) {
-        Log.trace( "Enter - DoubleRecordMailServiceClient.checkAndSendMails()" );
-
+var DoubleRecordMailServiceClient = function () {
+    function sendMessage(subject, body) {
+        Log.trace("Enter - DoubleRecordMailServiceClient.checkAndSendMails()");
         try {
-            DoubleRecordMailServiceClientCore.sendMessage( subject, body );
-        }
-        finally {
-            Log.trace( "Exit - DoubleRecordMailServiceClient.checkAndSendMails()" );
+            DoubleRecordMailServiceClientCore.sendMessage(subject, body);
+        } finally {
+            Log.trace("Exit - DoubleRecordMailServiceClient.checkAndSendMails()");
         }
     }
 
