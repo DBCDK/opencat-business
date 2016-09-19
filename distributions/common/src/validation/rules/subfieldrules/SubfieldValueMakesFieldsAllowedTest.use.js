d227b5a3ac1d977b2de8a216d022b97767a34f8a
Morten von Staffeldt <mvs@dbc.dk>
2016-09-19 11:03:11 +0000

    fix story 1992, regarding new validation rules where a certain subfield value makes other fields allowed
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@104243 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ distributions/common/src/validation/rules/subfieldrules/SubfieldValueMakesFieldsAllowedTest.use.js
@@ -0,0 +1,83 @@
+//-----------------------------------------------------------------------------
+/**
+ * This file contains unittests for the SubfieldValueMakesFieldsAllowed module.
+ */
+
+//-----------------------------------------------------------------------------
+use( "ResourceBundle" );
+use( "SafeAssert" );
+use( "UnitTest" );
+use ( 'GenericSettings' );
+use( "SubfieldValueMakesFieldsAllowed" );
+//-----------------------------------------------------------------------------
+
+UnitTest.addFixture( "SubfieldValueMakesFieldsAllowed.validateSubfield", function( ) {
+    var bundle = ResourceBundleFactory.getBundle( SubfieldValueMakesFieldsAllowed.__BUNDLE_NAME );
+
+    var record = {
+        fields : [{
+            'name' : '032'
+        }, {
+            'name' : '996'
+        }]
+    };
+
+    var subfield = {
+        'name' : "a", 'value' : ""
+    };
+
+    var params = ["032", "996"]
+
+    var errMsg = ResourceBundle.getStringFormat( bundle, "subfield.value.makes.field.allowed.rule.error", "032", "a" );
+    SafeAssert.equal( "1 SubfieldValueMakesFieldsAllowed.validateSubfield invalid value", SubfieldValueMakesFieldsAllowed.validateSubfield( record, {}, subfield , params),  [ValidateErrors.subfieldError( "TODO:fixurl", errMsg )] );
+
+
+    var record = {
+        fields : [{
+            'name' : '032'
+        }, {
+            'name' : '996'
+        }]
+    };
+
+    var subfield = {
+        'name' : "a", 'value' : "DBC"
+    };
+
+    var params = ["032", "996"]
+
+    SafeAssert.equal( "2 SubfieldValueMakesFieldsAllowed.validateSubfield valid value", SubfieldValueMakesFieldsAllowed.validateSubfield( record, {}, subfield , params),  [] );
+
+
+    var record = {
+        fields : [{
+            'name' : '010'
+        }, {
+            'name' : '011'
+        }]
+    };
+
+    var subfield = {
+        'name' : "a", 'value' : ""
+    };
+
+    var params = ["032", "996"]
+
+    SafeAssert.equal( "1 SubfieldValueMakesFieldsAllowed.validateSubfield invalid value", SubfieldValueMakesFieldsAllowed.validateSubfield( record, {}, subfield , params),  [] );
+
+    var record = {
+        fields : [{
+            'name' : '010'
+        }, {
+            'name' : '011'
+        }]
+    };
+
+    var subfield = {
+        'name' : "a", 'value' : "DBC"
+    };
+
+    var params = ["032", "996"]
+
+    SafeAssert.equal( "1 SubfieldValueMakesFieldsAllowed.validateSubfield invalid value", SubfieldValueMakesFieldsAllowed.validateSubfield( record, {}, subfield , params),  [] );
+});
