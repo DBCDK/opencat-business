e917035eb6c61560111d33f074a2dfb7af6961eb
Thomas Hesselholt Lang <thl@dbc.dk>
2016-09-30 06:16:54 +0000

    File cleanup
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@104617 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- distributions/common/src/validation/rules/subfieldrules/SubfieldCannotContainValueTest.use.js
+++ distributions/common/src/validation/rules/subfieldrules/SubfieldCannotContainValueTest.use.js
@@ -1,9 +1,3 @@
-//-----------------------------------------------------------------------------
-/**
- * This file contains unittests for the SubfieldCannotContainValue module.
- */
-
-//-----------------------------------------------------------------------------
 use("DanMarc2Converter");
 use("GenericSettings");
 use("RecordUtil");
@@ -11,67 +5,58 @@ use("ResourceBundle");
 use("SafeAssert");
 use("SubfieldCannotContainValue");
 use("UnitTest");
-//-----------------------------------------------------------------------------
 
 UnitTest.addFixture("SubfieldCannotContainValue.validateSubfield", function () {
-    Log.trace("Enter - TestFixture: SubfieldCannotContainValue.validateSubfield");
+    var bundle = ResourceBundleFactory.getBundle(SubfieldCannotContainValue.__BUNDLE_NAME);
+    var record;
+    var field;
+    var subfield;
+    var error;
+    var params;
 
-    try {
-        var bundle = ResourceBundleFactory.getBundle(SubfieldCannotContainValue.__BUNDLE_NAME);
+    record = DanMarc2Converter.convertFromDanMarc2(RecordUtil.createFromString("001 00 *a 50984508 *b 710100 *c 42"));
+    field = record.fields[0];
+    subfield = field.subfields[2];
+    var errorMsg = [{
+        type: "ERROR",
+        params: {
+            param: [{
+                key: "message",
+                value: ResourceBundle.getStringFormat(bundle, "subfield.cannot.contain.value.rule.error", "c", "42")
+            }, {
+                key: "url",
+                value: "TODO:fixurl"
+            }, {
+                key: "pid",
+                value: "50984508:710100"
+            }]
+        }
+    }];
 
-        var record;
-        var field;
-        var subfield;
-        var error;
-        var params;
+    params = {values: []};
+    SafeAssert.equal("1. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), []);
 
-        record = DanMarc2Converter.convertFromDanMarc2(RecordUtil.createFromString("001 00 *a 50984508 *b 710100 *c 42"));
-        field = record.fields[0];
-        subfield = field.subfields[2];
-        var errorMsg = [{
-            type: "ERROR",
-            params: {
-                param: [{
-                    key: "message",
-                    value: ResourceBundle.getStringFormat(bundle, "subfield.cannot.contain.value.rule.error", "c", "42")
-                }, {
-                    key: "url",
-                    value: "TODO:fixurl"
-                }, {
-                    key: "pid",
-                    value: "50984508:710100"
-                }]
-            }
-        }];
+    params = {values: ["42"]};
+    SafeAssert.equal("2. subfieldCannotContainValue with value that is not allowed", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
 
-        params = {values: []};
-        SafeAssert.equal("1. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), []);
+    params = {values: [42]};
+    SafeAssert.equal("3. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
 
-        params = {values: ["42"]};
-        SafeAssert.equal("2. subfieldCannotContainValue with value that is not allowed", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
+    params = {values: ["30", "x"]};
+    SafeAssert.equal("4. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), []);
 
-        params = {values: [42]};
-        SafeAssert.equal("3. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
+    params = {values: ["42", "x"]};
+    SafeAssert.equal("5. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
 
-        params = {values: ["30", "x"]};
-        SafeAssert.equal("4. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), []);
+    params = {values: ["42", "x"]};
+    SafeAssert.equal("6. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
 
-        params = {values: ["42", "x"]};
-        SafeAssert.equal("5. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
+    params = {values: ["42", "x"]};
+    SafeAssert.equal("7. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
 
-        params = {values: ["42", "x"]};
-        SafeAssert.equal("6. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
+    params = {values: ["42", "x"], notcondition: {subfield: "001b", value: "710100"}};
+    SafeAssert.equal("8. subfieldCannotContainValue meet condition", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), []);
 
-        params = {values: ["42", "x"]};
-        SafeAssert.equal("7. subfieldCannotContainValue with empty params array", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
-
-        params = {values: ["42", "x"], notcondition: {subfield: "001b", value: "710100"}};
-        SafeAssert.equal("8. subfieldCannotContainValue meet condition", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), []);
-
-        params = {values: ["42", "x"], notcondition: {subfield: "001b", value: "870970"}};
-        SafeAssert.equal("9. subfieldCannotContainValue does not meet condition", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
-    }
-    finally {
-        Log.trace("Exit - TestFixture: SubfieldCannotContainValue.validateSubfield");
-    }
+    params = {values: ["42", "x"], notcondition: {subfield: "001b", value: "870970"}};
+    SafeAssert.equal("9. subfieldCannotContainValue does not meet condition", SubfieldCannotContainValue.validateSubfield(record, field, subfield, params), errorMsg);
 });
