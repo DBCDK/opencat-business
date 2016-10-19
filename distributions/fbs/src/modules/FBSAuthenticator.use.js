1a1a1ea668a6f63429a8e83adb6b70e2dba357ff
Thomas Hesselholt Lang <thl@dbc.dk>
2016-10-19 08:58:51 +0000

    File cleanup
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@105142 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- distributions/fbs/src/modules/FBSAuthenticator.use.js
+++ distributions/fbs/src/modules/FBSAuthenticator.use.js
@@ -1,22 +1,19 @@
-//-----------------------------------------------------------------------------
-use( "DanMarc2Converter" );
-use( "DefaultAuthenticator" );
-use( "Log" );
-use( "Marc" );
-use( "MarcClasses" );
-use( "NoteAndSubjectExtentionsHandler" );
-use( "RawRepoClient" );
-use( "RecordUtil" );
-use( "ResourceBundle" );
-use( "ResourceBundleFactory" );
-use( "UpdateConstants" );
-use( "UpdateOwnership" );
-use( "ValidateErrors" );
+use("DanMarc2Converter");
+use("DefaultAuthenticator");
+use("Log");
+use("Marc");
+use("MarcClasses");
+use("NoteAndSubjectExtentionsHandler");
+use("RawRepoClient");
+use("RecordUtil");
+use("ResourceBundle");
+use("ResourceBundleFactory");
+use("UpdateConstants");
+use("UpdateOwnership");
+use("ValidateErrors");
 
-//-----------------------------------------------------------------------------
-EXPORTED_SYMBOLS = [ 'FBSAuthenticator' ];
+EXPORTED_SYMBOLS = ['FBSAuthenticator'];
 
-//-----------------------------------------------------------------------------
 /**
  * Module to contain entry points for the authenticator API between Java and
  * JavaScript.
@@ -24,7 +21,7 @@ EXPORTED_SYMBOLS = [ 'FBSAuthenticator' ];
  * @namespace
  * @name FBSAuthenticator
  */
-var FBSAuthenticator = function() {
+var FBSAuthenticator = function () {
     var __BUNDLE_NAME = "fbs-auth";
 
     /**
@@ -39,24 +36,19 @@ var FBSAuthenticator = function() {
      *
      * @name FBSAuthenticator#authenticateRecord
      */
-    function authenticateRecord( record, userId, groupId, settings ) {
-        Log.trace( "Enter - FBSAuthenticator.authenticateRecord( ", record, ", ", userId, ", ", groupId, ", ", settings, " )" );
-
+    function authenticateRecord(record, userId, groupId, settings) {
+        Log.trace("Enter - FBSAuthenticator.authenticateRecord(", record, ", ", userId, ", ", groupId, ", ", settings, ")");
         var result = undefined;
         try {
-            if( settings !== undefined ) {
-                ResourceBundleFactory.init( settings );
+            if (settings !== undefined) {
+                ResourceBundleFactory.init(settings);
             }
-
             var authenticator = DefaultAuthenticator.create();
-
-            var marcRecord = DanMarc2Converter.convertToDanMarc2( JSON.parse( record ) );
-            var result = authenticator.authenticateRecord( marcRecord, userId, groupId );
-
-            return JSON.stringify( result );
-        }
-        finally {
-            Log.trace( "Exit - FBSAuthenticator.authenticateRecord(): result" );
+            var marcRecord = DanMarc2Converter.convertToDanMarc2(JSON.parse(record));
+            result = authenticator.authenticateRecord(marcRecord, userId, groupId);
+            return JSON.stringify(result);
+        } finally {
+            Log.trace("Exit - FBSAuthenticator.authenticateRecord(): " + result);
         }
     }
 
@@ -67,107 +59,91 @@ var FBSAuthenticator = function() {
      *
      * @returns {Array} A list of records of type Record.
      */
-    function recordDataForRawRepo( record, userId, groupId ) {
-        Log.trace( StringUtil.sprintf( "Enter - FBSAuthenticator.recordDataForRawRepo( %s, %s, %s )", record, userId, groupId ) );
-
+    function recordDataForRawRepo(record, userId, groupId) {
+        Log.trace("Enter - FBSAuthenticator.recordDataForRawRepo(", record, ", ", userId, ", ", groupId, ")");
         var result = [];
         try {
-            var records = __recordDataForRawRepo( record, userId, groupId );
-
-            for( var i = 0; i < records.length; i++ ) {
-                var curRecord = records[ i ];
-                curRecord = RecordUtil.addOrReplaceSubfield( curRecord, "001", "c", RecordUtil.currentAjustmentTime() );
-                Log.debug( "curRecord:\n", curRecord );
-
-                Log.debug( "Adding curRecord: ", curRecord.toString() );
-                result.push( curRecord );
+            var records = __recordDataForRawRepo(record, userId, groupId);
+            for (var i = 0; i < records.length; i++) {
+                var curRecord = records[i];
+                curRecord = RecordUtil.addOrReplaceSubfield(curRecord, "001", "c", RecordUtil.currentAjustmentTime());
+                Log.debug("curRecord:\n", curRecord);
+                Log.debug("Adding curRecord: ", curRecord.toString());
+                result.push(curRecord);
             }
-
             return result;
-        }
-        finally {
-            Log.trace( StringUtil.sprintf( "Exit - FBSAuthenticator.recordDataForRawRepo( %s )", result ) );
+        } finally {
+            Log.trace("Exit - FBSAuthenticator.recordDataForRawRepo(", record, ")");
         }
     }
 
-    function __recordDataForRawRepo( record, userId, groupId ) {
-        Log.trace( "Enter - FBSAuthenticator.__recordDataForRawRepo()" );
-
+    function __recordDataForRawRepo(record, userId, groupId) {
+        Log.trace("Enter - FBSAuthenticator.__recordDataForRawRepo()");
         try {
-            var agencyId = record.getValue( /001/, /b/ );
-
+            var agencyId = record.getValue(/001/, /b/);
             if (agencyId === UpdateConstants.COMMON_AGENCYID) {
-                return __recordDataForRawRepoCommonRecord( record, userId, groupId );
+                return __recordDataForRawRepoCommonRecord(record, userId, groupId);
             }
-
-            return [ record ];
-        }
-        finally {
-            Log.trace( "Exit - FBSAuthenticator.__recordDataForRawRepo()" );
+            return [record];
+        } finally {
+            Log.trace("Exit - FBSAuthenticator.__recordDataForRawRepo()");
         }
     }
 
-    function __recordDataForRawRepoCommonRecord( record, userId, groupId ) {
-        Log.trace( "Enter - FBSAuthenticator.__recordDataForRawRepoCommonRecord" );
-
+    function __recordDataForRawRepoCommonRecord(record, userId, groupId) {
+        Log.trace("Enter - FBSAuthenticator.__recordDataForRawRepoCommonRecord");
         var correctedRecord = null;
         var dbcEnrichmentRecord = null;
         try {
-            RecordUtil.addOrReplaceSubfield( record, "001", "b", UpdateConstants.RAWREPO_COMMON_AGENCYID );
+            RecordUtil.addOrReplaceSubfield(record, "001", "b", UpdateConstants.RAWREPO_COMMON_AGENCYID);
 
-            correctedRecord = NoteAndSubjectExtentionsHandler.recordDataForRawRepo( record, userId, groupId );
+            correctedRecord = NoteAndSubjectExtentionsHandler.recordDataForRawRepo(record, userId, groupId);
 
-            var recId = correctedRecord.getValue( /001/, /a/ );
+            var recId = correctedRecord.getValue(/001/, /a/);
             var agencyId = UpdateConstants.RAWREPO_COMMON_AGENCYID;
             var curRecord;
-            if( RawRepoClient.recordExists( recId, agencyId ) ) {
-                curRecord = RawRepoClient.fetchRecord( recId, agencyId );
-            }
-            else {
+            if (RawRepoClient.recordExists(recId, agencyId)) {
+                curRecord = RawRepoClient.fetchRecord(recId, agencyId);
+            } else {
                 curRecord = undefined;
             }
-            correctedRecord = UpdateOwnership.mergeRecord( correctedRecord, curRecord );
+            correctedRecord = UpdateOwnership.mergeRecord(correctedRecord, curRecord);
 
-            var owner = correctedRecord.getValue( /996/, /a/ );
-            if( owner === "" ) {
-                Log.debug( "No owner in record." );
-                return [ correctedRecord ];
+            var owner = correctedRecord.getValue(/996/, /a/);
+            if (owner === "") {
+                Log.debug("No owner in record.");
+                return [correctedRecord];
             }
 
-            var recId = correctedRecord.getValue( /001/, /a/ );
+            recId = correctedRecord.getValue(/001/, /a/);
 
-            if( !RawRepoClient.recordExists( recId, UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID ) ) {
-                Log.debug( "DBC enrichment record [", recId, ":", UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID, "] does not exist." );
+            if (!RawRepoClient.recordExists(recId, UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID)) {
+                Log.debug("DBC enrichment record [", recId, ":", UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID, "] does not exist.");
                 dbcEnrichmentRecord = new Record;
-
-                var idField = record.field( "001" ).clone();
-                idField.subfield( "b" ).value = UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID;
-
-                dbcEnrichmentRecord.append( idField );
-            }
-            else {
+                var idField = record.field("001").clone();
+                idField.subfield("b").value = UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID;
+                dbcEnrichmentRecord.append(idField);
+            } else {
                 Log.debug("DBC enrichment record [", recId, ":", UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID, "found.");
                 dbcEnrichmentRecord = RawRepoClient.fetchRecord(recId, UpdateConstants.RAWREPO_DBC_ENRICHMENT_AGENCY_ID);
             }
 
-            var recordStatus = correctedRecord.getValue( /004/, /r/ );
-            if( recordStatus !== "" ) {
-                Log.debug( "Replace 004 *r in DBC enrichment record with: ", recordStatus );
-                dbcEnrichmentRecord = RecordUtil.addOrReplaceSubfield( dbcEnrichmentRecord, "004", "r", recordStatus );
+            var recordStatus = correctedRecord.getValue(/004/, /r/);
+            if (recordStatus !== "") {
+                Log.debug("Replace 004 *r in DBC enrichment record with: ", recordStatus);
+                dbcEnrichmentRecord = RecordUtil.addOrReplaceSubfield(dbcEnrichmentRecord, "004", "r", recordStatus);
             }
 
-            var recordType = correctedRecord.getValue( /004/, /a/ );
-            if( recordType !== "" ) {
-                Log.debug( "Replace 004 *a in DBC enrichment record with: ", recordType );
-                dbcEnrichmentRecord = RecordUtil.addOrReplaceSubfield( dbcEnrichmentRecord, "004", "a", recordType );
+            var recordType = correctedRecord.getValue(/004/, /a/);
+            if (recordType !== "") {
+                Log.debug("Replace 004 *a in DBC enrichment record with: ", recordType);
+                dbcEnrichmentRecord = RecordUtil.addOrReplaceSubfield(dbcEnrichmentRecord, "004", "a", recordType);
             }
-
-            return [ correctedRecord, dbcEnrichmentRecord ];
-        }
-        finally {
-            Log.trace( "  correctedRecord:\n", correctedRecord );
-            Log.trace( "  dbcEnrichmentRecord:\n", dbcEnrichmentRecord );
-            Log.trace( "Exit - FBSAuthenticator.__recordDataForRawRepoCommonRecord" );
+            return [correctedRecord, dbcEnrichmentRecord];
+        } finally {
+            Log.trace("  correctedRecord:\n", correctedRecord);
+            Log.trace("  dbcEnrichmentRecord:\n", dbcEnrichmentRecord);
+            Log.trace("Exit - FBSAuthenticator.__recordDataForRawRepoCommonRecord");
         }
     }
 
