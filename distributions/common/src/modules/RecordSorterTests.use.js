07cb1abfb715936b8598ae1992518f9cda9aad60
Morten von Staffeldt <mvs@dbc.dk>
2016-11-03 15:21:25 +0000

    test fixed for RecordSorter
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@105572 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- distributions/common/src/modules/RecordSorterTests.use.js
+++ distributions/common/src/modules/RecordSorterTests.use.js
@@ -11,7 +11,7 @@ UnitTest.addFixture("RecordSorter.insertField", function () {
 
     record = new Record();
     field = expected.field(0);
-    Assert.equalValue("Field into empty record", RecordSorter.insertField(record, field), expected);
+    Assert.equalValue("Field into empty record", RecordSorter.insertField(record, field).toString(), expected.toString());
 
     expected = new Record();
     expected.fromString(
@@ -24,7 +24,7 @@ UnitTest.addFixture("RecordSorter.insertField", function () {
         "004 00 *a text"
     );
     field = expected.field(0);
-    Assert.equalValue("Field into first position", RecordSorter.insertField(record, field), expected);
+    Assert.equalValue("Field into first position", RecordSorter.insertField(record, field).toString(), expected.toString());
 
     expected = new Record();
     expected.fromString(
@@ -39,7 +39,7 @@ UnitTest.addFixture("RecordSorter.insertField", function () {
         "245 00 *a title"
     );
     field = expected.field(1);
-    Assert.equalValue("Field into middle position", RecordSorter.insertField(record, field), expected);
+    Assert.equalValue("Field into middle position", RecordSorter.insertField(record, field).toString(), expected.toString());
 
     expected = new Record();
     expected.fromString(
@@ -54,27 +54,40 @@ UnitTest.addFixture("RecordSorter.insertField", function () {
         "004 00 *a r\n"
     );
     field = expected.field(2);
-    Assert.equalValue("Field into last position", RecordSorter.insertField(record, field), expected);
+    Assert.equalValue("Field into last position", RecordSorter.insertField(record, field).toString(), expected.toString());
 
     expected = new Record();
     expected.fromString(
         "001 00 *a text\n" +
         "004 00 *a r\n" +
-        "245 00 *a title a" +
-        "245 00 *a title b" +
-        "245 00 *a title c" +
+        "245 00 *a title a\n" +
+        "245 00 *a title b\n" +
+        "245 00 *a title c\n" +
         "245 00 *a new field"
     );
+/*
+ Test number : 4
+ URL         : file:///home/mvs/development/opencat-business/distributions/common/src/modules/RecordSorterTests.use.js
+ Description : Field into record with repeated fields
+ Evaluated   : (new String("  \n001 00 *a text \n004 00 *a r \n245 00 *a title a245 00 *a title b245 00 *a title c \n"))
+ Time        : 0.011
+ Expected    : (new String("001 00 *a text \n004 00 *a r \n245 00 *a title a245 00 *a title b245 00 *a title c245 00 *a new field \n")) (string)
+ Actual      : (new String("  \n001 00 *a text \n004 00 *a r \n245 00 *a title a245 00 *a title b245 00 *a title c \n")) (string)
+ String diff : Expected and actual differs at position 13:
+ Expected: (new String("001 00 *a text \n004 00 *a r \n245 00 *a title a245 00 *a title b24...
+ Actual  : (new String("  \n001 00 *a text \n004 00 *a r \n245 00 *a title a245 00 *a title...
+ */
+
 
     record = new Record();
     record.fromString(
         "001 00 *a text\n" +
         "004 00 *a r\n" +
-        "245 00 *a title a" +
-        "245 00 *a title b" +
+        "245 00 *a title a\n" +
+        "245 00 *a title b\n" +
         "245 00 *a title c"
     );
     field = expected.field(5);
-    Assert.equalValue("Field into record with repeated fields", RecordSorter.insertField(record, field), expected);
+    Assert.equalValue("Field into record with repeated fields", RecordSorter.insertField(record, field).toString(), expected.toString());
 
 });
