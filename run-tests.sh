cf255cf2f00ab9480b6cdef8ec4ad2775fbaf2d3
Morten von Staffeldt <mvs@dbc.dk>
2017-01-05 14:44:03 +0000

    added rev number to run tests
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@108397 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- run-tests.sh
+++ run-tests.sh
@@ -5,20 +5,20 @@ function die() {
   exit 1
 }
 
-#rm -rf ocb-tools-1.0.0 2>/dev/null
-#rm ocb-tools-1.0.0.* 2>/dev/null
-#
-#wget -q http://is.dbc.dk/job/ocb-tools-head/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tar.gz || die "wget -q http://is.dbc.dk/job/ocb-tools-head/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tar.gz"
-#tar -xf ocb-tools-1.0.0.tar.gz || die "tar -xf ocb-tools-1.0.0.tar.gz"
-#
-#template-transpiler/bin/transpile-templates.sh || die "template-transpiler/bin/transpile-templates.sh"
-#
-#/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh js-tests  || die "ocb-test.sh js-tests"
-#/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a u -s || die "ocb-test.sh run -a u -s"
-#/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a b -s || die "ocb-test.sh run -a b -s"
-#
-#rm -rf deploy || die "Failed to remove deploy"
-#
+rm -rf ocb-tools-1.0.0 2>/dev/null
+rm ocb-tools-1.0.0.* 2>/dev/null
+
+wget -q http://is.dbc.dk/job/ocb-tools-head/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tar.gz || die "wget -q http://is.dbc.dk/job/ocb-tools-head/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tar.gz"
+tar -xf ocb-tools-1.0.0.tar.gz || die "tar -xf ocb-tools-1.0.0.tar.gz"
+
+template-transpiler/bin/transpile-templates.sh || die "template-transpiler/bin/transpile-templates.sh"
+
+/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh js-tests  || die "ocb-test.sh js-tests"
+/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a u -s || die "ocb-test.sh run -a u -s"
+/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a b -s || die "ocb-test.sh run -a b -s"
+
+rm -rf deploy || die "Failed to remove deploy"
+
 mkdir -p deploy/opencat-business || die "mkdir -p deploy/opencat-business"
 cp -r bin deploy/opencat-business/ || die "cp -r bin deploy/opencat-business/"
 cp -r distributions deploy/opencat-business/ || die "cp -r distributions deploy/opencat-business/"
