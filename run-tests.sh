b733e35f246ea621313c5730e1497a3facc5fbe9
Thomas Hesselholt Lang <thl@dbc.dk>
2017-01-11 06:59:01 +0000

    Re-inserted code after corrections.
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@108644 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- run-tests.sh
+++ run-tests.sh
@@ -14,9 +14,8 @@ tar -xf ocb-tools-1.0.0.tar.gz || die "tar -xf ocb-tools-1.0.0.tar.gz"
 template-transpiler/bin/transpile-templates.sh || die "template-transpiler/bin/transpile-templates.sh"
 
 /bin/bash ocb-tools-1.0.0/bin/ocb-test.sh js-tests  || die "ocb-test.sh js-tests"
-# DISABLED UNTIL THL FIXES OCB-TOOLS 01102017
-#/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a u -s || die "ocb-test.sh run -a u -s"
-#/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a b -s || die "ocb-test.sh run -a b -s"
+/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a u -s || die "ocb-test.sh run -a u -s"
+/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh run -a b -s || die "ocb-test.sh run -a b -s"
 
 rm -rf deploy || die "Failed to remove deploy"
 
