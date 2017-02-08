79791ede5c15c98e9e9777f064498234bde6a1d3
Michel Berggren <mib@dbc.dk>
2017-02-08 11:30:15 +0000

    Cleanup of obsolete testscripts and some reorganization
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@109841 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ bin/clean_ocblog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Script that makes the ocb-test.log file readable - needs adjustment from time to time 
+
+cat ocb-test.log |\
+egrep -v 'ModuleHandler|cannot read file|ield rule|Log4JStopWatch|FileSchemeHandler|Environment.java|ClasspathSchemeHandler.java|microseconds|milliseconds.|^$' |\
+egrep -v 'OCBFileSystem.java:81' |\
+sed -e 's&ServiceScripter.java&&' |\
+sed -e 's&d.d.o.s.ServiceScripter&&' |\
+sed -e 's&file:///home/${USER}/iscrum/opencat-business/distributions/common/src/&&' |\
+sed -e 's&jar:file:/home/${USER}/iscrum/opencat-business/ocb-tools-1.0.0/bin/ocb-test-1.0-SNAPSHOT-jar-with-dependencies.jar!/javascript/javacore/LogCore.use.js:50&&'|\
+sed -e 's&jar:file:/home/${USER}/iscrum/opencat-business/ocb-tools-1.0.0/bin/ocb-test-1.0-SNAPSHOT-jar-with-dependencies.jar!/javascript/javacore/LogCore.use.js:51&&'|\
+sed -e 's&jar:file:/home/${USER}/iscrum/opencat-business/ocb-tools-1.0.0/bin/ocb-test-1.0-SNAPSHOT-jar-with-dependencies.jar!/javascript/javacore/LogCore.use.js:52&&'|\
+sed -e 's&jar:file:/home/${USER}/iscrum/opencat-business/ocb-tools-1.0.0/bin/ocb-test-1.0-SNAPSHOT-jar-with-dependencies.jar!/javascript/javacore/LogCore.use.js:49&&' |\
+sed -e 's&jar:file:/home/${USER}/iscrum/ocb-tools/target/dist/ocb-tools-1.0.0/bin/ocb-test-1.0-SNAPSHOT-jar-with-dependencies.jar!/javascript/javacore/LogCore.use.js:48&&' |\
+sed -e 's&jar:file:/home/${USER}/iscrum/ocb-tools/target/dist/ocb-tools-1.0.0/bin/ocb-test-1.0-SNAPSHOT-jar-with-dependencies.jar!/javascript/javacore/LogCore.use.js:51&&' |\
+sed -e 's&jar:file:/home/${USER}/iscrum/ocb-tools/target/dist/ocb-tools-1.0.0/bin/ocb-test-1.0-SNAPSHOT-jar-with-dependencies.jar!/javascript/javacore/LogCore.use.js:49&&' > ocb-test.log.vi
+vi ocb-test.log.vi
--- distributions/tests/test_templates.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SHELL_CMD=dbc-jsshell
-SEARCH_PATH_ARGS="-a file:. -a file:../rules -a file:../modules -a file:../modules/unittests -l jstest.log -L info" 
-
-$SHELL_CMD $SEARCH_PATH_ARGS TemplateTest.js
