701f180c4852afe0ee9d049b4cae1b255dc47d92
Anders Thrane Michelsen <atm@dbc.dk>
2017-02-13 16:29:47 +0000

    Fikset sti til template-transpiler
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@110063 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -11,7 +11,7 @@ rm ocb-tools-1.0.0.* 2>/dev/null
 wget -q http://is.dbc.dk/job/ocb-tools-head/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tar.gz || die "wget -q http://is.dbc.dk/job/ocb-tools-head/lastSuccessfulBuild/artifact/target/dist/ocb-tools-1.0.0.tar.gz"
 tar -xf ocb-tools-1.0.0.tar.gz || die "tar -xf ocb-tools-1.0.0.tar.gz"
 
-../template-transpiler/bin/transpile-templates.sh || die "template-transpiler/bin/transpile-templates.sh"
+template-transpiler/bin/transpile-templates.sh || die "template-transpiler/bin/transpile-templates.sh"
 
 /bin/bash ocb-tools-1.0.0/bin/ocb-test.sh js-tests  || die "/bin/bash ocb-tools-1.0.0/bin/ocb-test.sh js-tests"
 
