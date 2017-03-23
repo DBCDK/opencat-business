752ed11fba0ead0c103725837db15ab7daa23266
Morten von Staffeldt <mvs@dbc.dk>
2017-03-23 11:45:14 +0000

    added new validationRule MustContainOneOfFields according to us:2143
    git-svn-id: https://svn.dbc.dk/repos/opencat-business/trunk@111582 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ distributions/common/src/validation/rules/recordrules/MustContainOneOfFields.use.js
@@ -0,0 +1,54 @@
+use("Exception");
+use("Log");
+use("ResourceBundle");
+use("ResourceBundleFactory");
+use("TemplateUrl");
+use("ValidateErrors");
+use("ValueCheck");
+use("ValidationUtil");
+
+EXPORTED_SYMBOLS = ['MustContainOneOfFields'];
+
+var MustContainOneOfFields = function () {
+    var BUNDLE_NAME = "validation";
+
+    /**
+     * MustContainOneOfFields checks if a record contains the field in the params array.
+     * @syntax RecordRules.MustContainOneOf (  record, params  )
+     * @param {Object} record The record as a json.
+     * @param {Object} An object with the key fields and Arrays as value , containing field numbers of which one must exists in the record to pass validation
+     * @name MustContainOneOfFields.validateRecord
+     * @return an array which is empty with no errors present, or contains the appropiate errors
+     * @method
+     */
+    function validateRecord(record, params) {
+        Log.trace("Enter - MustContainOneOfFields.validateRecord( ", record, ", ", params, " )");
+        var result = [];
+        try {
+            var bundle = ResourceBundleFactory.getBundle(BUNDLE_NAME);
+            ValueCheck.check("params.fields", params.fields).instanceOf(Array);
+            ValueCheck.checkThat("params.fields", params.fields.length).is.greaterThan(0);
+            if (!__checkRecordForField (record, params)) {
+                var msg = ResourceBundle.getStringFormat(bundle, "record.must.contain.one.of.fields.error", params.fields);
+                result.push(ValidateErrors.recordError("", msg));
+            }
+            return result;
+        } finally {
+            Log.trace("Exit - MustContainOneOfFields.validateRecord: ", result);
+        }
+    }
+
+    var __checkRecordForField = function (record, params) {
+        for (var i = 0; i < params.fields.length; ++i) {
+            if (ValidationUtil.recordContainsField(record, params.fields[i]) === true) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    return {
+        "validateRecord": validateRecord,
+        "__BUNDLE_NAME": BUNDLE_NAME
+    };
+}();
---
+++ distributions/common/src/validation/rules/recordrules/MustContainOneOfFieldsTest.use.js
@@ -0,0 +1,46 @@
+use( "SafeAssert" );
+use( "UnitTest" );
+use( "MustContainOneOfFields" );
+
+UnitTest.addFixture( "Test MustContainOneOfFields.validateRecord", function( ) {
+
+    var record = {
+        fields : [{
+            name : '001',
+            indicator : '00',
+            subfields : []
+        }, {
+            name : '002',
+            indicator : '00',
+            subfields : []
+        }, {
+            name : '003',
+            indicator : '00',
+            subfields : []
+        }]
+    };
+
+    var params = {
+        'fields' : ['001', '002', '003']
+    };
+    Assert.equal( "1 testing with valid " + params + " param", MustContainOneOfFields.validateRecord( record, params ), [] );
+
+    params = {
+        'fields' : []
+    };
+    var err = new Error("ValueCheckError: The following check failed: Value of 'params.fields' : '0' is not > '0'");
+
+    Assert.exception ("1 testing with valid " + params + " param", function(){MustContainOneOfFields.validateRecord( record, params )} , err);
+
+    params = {
+        'fields' : ['001', '004']
+    };
+    Assert.equal( "1 testing with valid " + params + " param", MustContainOneOfFields.validateRecord( record, params ), [] );
+
+    params = {
+        'fields' : ['004', '005']
+    };
+
+    var valErr =  [({type:"ERROR", urlForDocumentation:"", message:"En af felterne '004,005' skal forefindes i posten  "})];
+    Assert.equal( "1 testing with valid " + params + " param", MustContainOneOfFields.validateRecord( record, params ), valErr );
+} );
