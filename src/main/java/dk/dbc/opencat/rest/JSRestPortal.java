23a5b088bac0d6e85788164b4664e887540d51de
Jesper Bruun <jbr@dbc.dk>
2020-05-28 10:45:28 +0200

    Move away from single generic endpoint towards each js-method get its own endpoint.
    Auditors: atm, jbn, hwha, mib

--- src/main/java/dk/dbc/opencat/rest/JSRestPortal.java
+++ src/main/java/dk/dbc/opencat/rest/JSRestPortal.java
@@ -1,5 +1,7 @@
 package dk.dbc.opencat.rest;
 
+import dk.dbc.common.records.MarcRecord;
+import dk.dbc.dataio.jsonb.JSONBContext;
 import dk.dbc.dataio.jsonb.JSONBException;
 import dk.dbc.opencat.javascript.ScripterEnvironment;
 import dk.dbc.opencat.javascript.ScripterException;
@@ -8,6 +10,7 @@ import dk.dbc.opencat.ws.JNDIResources;
 import dk.dbc.util.Timed;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
@@ -21,45 +24,33 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import dk.dbc.dataio.jsonb.JSONBContext;
 
 @Stateless
 @Path("/api")
 public class JSRestPortal {
     private static final Logger LOGGER = LoggerFactory.getLogger(JSRestPortal.class);
-    private Properties settings = JNDIResources.getProperties();
-    private static JSONBContext jsonbContext = new JSONBContext();
+    private final Properties settings = JNDIResources.getProperties();
 
     @EJB
     ScripterPool scripterPool;
 
     @POST
-    @Path("v1/{methodName}")
+    @Path("v1/validateRecord")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     @Timed
-    public String callJSMethod(@PathParam("methodName") String methodName,String[] callParams) throws InterruptedException, ScripterException, JSONBException {
-        LOGGER.info("JS method to call:{}", methodName);
-        LOGGER.info("Incoming params: {}", String.join(",",callParams));
+    public Object validateRecord(String[] callParams) throws InterruptedException, ScripterException {
         ScripterEnvironment scripterEnvironment = null;
+        List<Object> parms = new ArrayList<Object>(Arrays.asList(callParams));
+        parms.add(settings);
         Object result;
-        final List<Object> params = new ArrayList<>();
-        for (String callParam: callParams) {
-            if ("__settings".equals(callParam)) {
-                params.add(JNDIResources.getProperties());
-            }
-            else {
-                params.add(callParam);
-            }
-        }
-        LOGGER.info("Rewritten parms: {}", params);
         try {
             scripterEnvironment = scripterPool.take();
-            result = scripterEnvironment.callMethod(methodName, params.toArray());
+            result = scripterEnvironment.callMethod("validateRecord", parms.toArray());
         }
         finally {
             scripterPool.put(scripterEnvironment);
         }
-        return jsonbContext.marshall(result);
+        return result;
     }
 }
