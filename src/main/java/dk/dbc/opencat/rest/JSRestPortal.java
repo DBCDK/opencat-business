1dccb29ee23d6922e852b0aed72ef539c9a30c2d
Jesper Bruun <jbr@dbc.dk>
2020-06-08 13:50:52 +0200

    Use newer JSONB package
    Auditors: atm, jbn, hwha, mib

--- src/main/java/dk/dbc/opencat/rest/JSRestPortal.java
+++ src/main/java/dk/dbc/opencat/rest/JSRestPortal.java
@@ -1,25 +1,20 @@
 package dk.dbc.opencat.rest;
 
-import dk.dbc.common.records.MarcRecord;
-import dk.dbc.dataio.jsonb.JSONBContext;
-import dk.dbc.dataio.jsonb.JSONBException;
+import dk.dbc.commons.jsonb.JSONBContext;
+import dk.dbc.commons.jsonb.JSONBException;
 import dk.dbc.opencat.javascript.ScripterEnvironment;
 import dk.dbc.opencat.javascript.ScripterException;
 import dk.dbc.opencat.javascript.ScripterPool;
 import dk.dbc.opencat.ws.JNDIResources;
+import dk.dbc.opencatbusiness.dto.ValidateRecordRequestDTO;
+import dk.dbc.updateservice.dto.MessageEntryDTO;
 import dk.dbc.util.Timed;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
-import java.util.stream.Collectors;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
@@ -30,27 +25,31 @@ import org.slf4j.LoggerFactory;
 public class JSRestPortal {
     private static final Logger LOGGER = LoggerFactory.getLogger(JSRestPortal.class);
     private final Properties settings = JNDIResources.getProperties();
+    private static final JSONBContext jsonbContext = new JSONBContext();
 
-    @EJB
-    ScripterPool scripterPool;
 
     @POST
     @Path("v1/validateRecord")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     @Timed
-    public Object validateRecord(String[] callParams) throws InterruptedException, ScripterException {
+    public String validateRecord(ValidateRecordRequestDTO validateRecordRequestDTO) throws InterruptedException, ScripterException, JSONBException {
+        ScripterPool scripterPool = new ScripterPool();
         ScripterEnvironment scripterEnvironment = null;
-        List<Object> parms = new ArrayList<Object>(Arrays.asList(callParams));
-        parms.add(settings);
-        Object result;
+        String result;
         try {
             scripterEnvironment = scripterPool.take();
-            result = scripterEnvironment.callMethod("validateRecord", parms.toArray());
+            result = (String) scripterEnvironment.callMethod("validateRecord",
+                    validateRecordRequestDTO.getTemplateName(),
+                    validateRecordRequestDTO.getRecord(),
+                    settings);
         }
         finally {
             scripterPool.put(scripterEnvironment);
         }
+        MessageEntryDTO messageEntryDTO = new MessageEntryDTO();
+        String messString = jsonbContext.marshall(messageEntryDTO);
+        LOGGER.info("Tester:{}", jsonbContext.unmarshall(messString, MessageEntryDTO.class));
         return result;
     }
 }
