cb10019f17b13c7471e00b90a959a0cae732f048
Anders Thrane Michelsen <atm@dbc.dk>
2020-12-14 12:24:28 +0100

    Update Rule package
    Auditors: jbn, jbr, mib

--- distributions/common/src/entrypoints/update/modules/OpenAgencyClientCore.use.js
+++ distributions/common/src/entrypoints/update/modules/OpenAgencyClientCore.use.js
@@ -21,14 +21,14 @@ var OpenAgencyClientCore = function () {
     }
 
     var features = {
-        create_enrichments: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.CREATE_ENRICHMENTS,
-        use_enrichments: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.USE_ENRICHMENTS,
-        auth_common_notes: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.AUTH_COMMON_NOTES,
-        auth_common_subjects: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.AUTH_COMMON_SUBJECTS,
-        auth_dbc_records: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.AUTH_DBC_RECORDS,
-        auth_public_lib_common_record: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.AUTH_PUBLIC_LIB_COMMON_RECORD,
-        auth_ret_record: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.AUTH_RET_RECORD,
-        auth_root: Packages.dk.dbc.openagency.client.LibraryRuleHandler.Rule.AUTH_ROOT
+        create_enrichments: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.CREATE_ENRICHMENTS,
+        use_enrichments: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.USE_ENRICHMENTS,
+        auth_common_notes: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.AUTH_COMMON_NOTES,
+        auth_common_subjects: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.AUTH_COMMON_SUBJECTS,
+        auth_dbc_records: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.AUTH_DBC_RECORDS,
+        auth_public_lib_common_record: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.AUTH_PUBLIC_LIB_COMMON_RECORD,
+        auth_ret_record: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.AUTH_RET_RECORD,
+        auth_root: Packages.dk.dbc.vipcore.libraryrules.VipCoreLibraryRulesConnector.Rule.Rule.AUTH_ROOT
     };
 
     function hasFeature(agencyId, featureName) {
