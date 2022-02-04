40d1b0c0d802e6fc90ced68dc49c94298ec87c8e
MichelLyck <mib@dbc.dk>
2022-02-04 16:25:19 +0100

    Handling of 665*v<data> now make a 666*0*h<data>

--- src/main/java/dk/dbc/opencat/transformation/MetaCompassHandler.java
+++ src/main/java/dk/dbc/opencat/transformation/MetaCompassHandler.java
@@ -1,7 +1,3 @@
-/*
- * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
- *  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
- */
 
 package dk.dbc.opencat.transformation;
 
