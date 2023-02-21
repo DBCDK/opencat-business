c95f44c72b795bcb2e50c4db05b77de14e4fca0d
MichelLyck <mib@dbc.dk>
2023-02-21 12:53:05 +0100

    Searching for weired testcase error in jenkins

--- distributions/common/src/modules/RecategorizationNoteFieldFactoryTests.use.js
+++ distributions/common/src/modules/RecategorizationNoteFieldFactoryTests.use.js
@@ -498,7 +498,8 @@ UnitTest.addFixture("RecategorizationNoteFieldFactory.newNoteField", function ()
         category: "# (DK5 65.821), materialekoder [a (xx)]. Postens opstilling ændret på grund af omkatalogisering fra periodica til flerbindsværk "
     };
 
-    Assert.equalValue("volume-to-single", callFunction(currentRecord, record).toString(), createNote(parts).toString());
+    Log.info("periodica-to-volume" + callFunction(currentRecord, record).toString());
+    Assert.equalValue("periodica-to-volume", callFunction(currentRecord, record).toString(), createNote(parts).toString());
 
     RawRepoClientCore.clear();
 });
