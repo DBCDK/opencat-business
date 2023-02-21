f73a9ff9d8290f8e7ce6cd896e0cd4b72e419d66
MichelLyck <mib@dbc.dk>
2023-02-21 15:14:41 +0100

    Searching for weired testcase error in jenkins

--- distributions/common/src/modules/RecategorizationNoteFieldFactory.use.js
+++ distributions/common/src/modules/RecategorizationNoteFieldFactory.use.js
@@ -296,10 +296,7 @@ var RecategorizationNoteFieldFactory = function () {
                     }
                 };
 
-                Log.debug("Formatting field: ", field);
-                Log.info("Message pre 009 : " + message);
                 message += ResourceBundle.getStringFormat(__loadBundle(), "note.category.type", ISBDFieldFormater.formatField(field, spec));
-                Log.info("Message post 009 : " + message);
             }
 
             if (message !== "") {
