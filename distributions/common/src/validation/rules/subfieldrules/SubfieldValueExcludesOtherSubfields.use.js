d97a94129d4adc2d0da41b1faf2c4ba76aa28b41
kpn <kpn@dbc.dk>
2023-11-29 14:08:24 +0100

    DA-2351 new validation rule

---
+++ distributions/common/src/validation/rules/subfieldrules/SubfieldValueExcludesOtherSubfields.use.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+ *  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+ */
+
+use("Exception");
+use("Log");
+use("ResourceBundle");
+use("ResourceBundleFactory");
+use("ValidateErrors");
+use("ValueCheck");
+
+EXPORTED_SYMBOLS = ['SubfieldValueExcludesOtherSubfields'];
+
+var SubfieldValueExcludesOtherSubfields = function () {
+    var BUNDLE_NAME = "validation";
+
+    /**
+     * This rule is used for requiring that a record does NOT contain one or more specific subfields if specific subfield value is present
+     * if the value of the subfield with this rule has a value matching the values of the rule
+     *
+     * Params should contain two elements:
+     * - values [Array] contains the values the subfield should match with
+     * - excludedSubfields [Array] contains the list of subfields that are excluded on the record if a match is made with
+     * the matchValues list
+     *
+     * @param record The whole record to perform the validation on
+     * @param field The field with the validation rule
+     * @param subfield The subfield with the validation rule
+     * @param params Contains the values to match and the excluded subfields
+     * @returns {Array}
+     */
+    function validateSubfield(record, field, subfield, params) {
+        Log.trace("Enter SubfieldValueExcludesOtherSubfields.validateSubField");
+        var result = [];
+
+        try {
+            ValueCheck.check("params", params);
+            ValueCheck.check("params", params).instanceOf(Object);
+            ValueCheck.check("params", params.values);
+            ValueCheck.check("params", params.values).instanceOf(Array);
+            ValueCheck.check("params", params.excludedSubfields);
+            ValueCheck.check("params", params.excludedSubfields).instanceOf(Array);
+
+            var matchValues = params.values;
+            var excludedSubfields = params.excludedSubfields;
+
+            if (matchValues.indexOf(subfield.value) > -1) {
+                record.fields.forEach(function (recordField) {
+                    if (excludedSubfields.indexOf(recordField.name) > -1) {
+                        var bundle = ResourceBundleFactory.getBundle(BUNDLE_NAME);
+                        var message = ResourceBundle.getStringFormat(bundle, "excluded.subfields", recordField.name, field.name, subfield.name);
+                        result.push(ValidateErrors.recordError("TODO:fixurl", message));
+                    }
+                });
+            }
+
+            return result;
+        } finally {
+            Log.trace("Exit SubfieldValueExcludesOtherSubfields.validateSubfield");
+        }
+    }
+
+    return {
+        '__BUNDLE_NAME': BUNDLE_NAME,
+        'validateSubfield': validateSubfield
+    };
+}();
